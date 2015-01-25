937a82b9b4b9711c5eb4a9c734da20eb83d0a9ec
KPrasch <kieranprasch@gmail.com>
2015-01-25 00:03:51 -0800

    adjustments

--- urls.py
+++ urls.py
@@ -21,7 +21,7 @@ urlpatterns = i18n_patterns("",
 
 urlpatterns += patterns('',
     url("^$", "views.main", name="home"),
-    url("^gallery/make_thumbs", "views.make_thumbs", name="home"),
+    url("^gallery", "views.gallery"),
 )
 
 # Adds ``STATIC_URL`` to the context of error pages, so that error
